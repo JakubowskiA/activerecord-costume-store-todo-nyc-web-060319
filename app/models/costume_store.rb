1178e8ae322f6076f33f1afc9ec56ff70d67ced7
JakubowskiA <arieljakubowski@gmail.com>
2019-06-14 16:27:57 -0400

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,11 @@
 # Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Base
+  # attr_accessor :name, :price, :size, :url
+
+  # def initialize(name, price, size, url)
+  #   @name = name
+  #   @price = price
+  #   @size = size
+  #   @url = url
+  # end
+end
